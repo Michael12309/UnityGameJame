106694fec114ae809fb45702a661c8e48f8bc783
Michael Leonard <michaelleonard5555@gmail.com>
2023-10-28 11:26:40 -0700

    player script

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -5,12 +5,17 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    private float alpha = .05f;
-    private Vector2 acc; //acceleration
-    private Vector2 velocity;
+    // private float alpha = .05f;
+    // private Vector2 acc; //acceleration
+    // private Vector2 velocity;
+    // private Rigidbody2D rb;
+    // //private float rotationSpeed = 5;
+    // private float rotation_x;
+
+    private float thrustInput;
+    private float turnInput;
     private Rigidbody2D rb;
-    //private float rotationSpeed = 5;
-    private float rotation_x;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,26 +24,28 @@ public class Player : MonoBehaviour
 
     public void OnMovevemt(InputAction.CallbackContext action)
     {
-        print(transform.rotation.eulerAngles.z);
-        float radians = (transform.rotation.eulerAngles.z + 90) * Mathf.Deg2Rad;
-        print(radians);
-        print(Mathf.Cos(radians) + "   :   " + Mathf.Sin(radians));
-        acc = new Vector2(Mathf.Cos(radians), Mathf.Sin(radians)) * alpha * action.action.ReadValue<Vector2>().y;
+        thrustInput = action.ReadValue<Vector2>().y;
+        
+        //float radians = (transform.rotation.eulerAngles.z + 90) * Mathf.Deg2Rad;
+        //acc = new Vector2(Mathf.Cos(radians), Mathf.Sin(radians)) * alpha * action.action.ReadValue<Vector2>().y;
     }
 
     public void OnRotation(InputAction.CallbackContext action)
     {
-        rotation_x = action.ReadValue<Vector2>().x;
-        print("rotation");
-        
-
+        turnInput = action.ReadValue<Vector2>().x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        velocity += acc;
-        rb.MovePosition(rb.position + velocity * Time.deltaTime);
-        transform.Rotate(new Vector3(0, 0, -rotation_x) * Time.deltaTime * 50, Space.World);
+        //velocity += acc;
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddRelativeForce(Vector2.up * thrustInput);
+        //rb.AddTorque(-turnInput);
+    //     rb.velocity = velocity;
+         rb.MoveRotation(rb.rotation + -turnInput);// * Time.fixedDeltaTime * 85);
     }
 }
