2fc511c6a777a92048a2c9f6fdea68a00461865b
JaylenLuc <JHLUC@UCI.EDU>
2023-10-28 00:54:09 -0700

    some mvm

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -5,21 +5,47 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    private float alpha = .05f;
+    private Vector2 acc; //acceleration
+    private Vector2 velocity;
+    private Rigidbody2D rb;
+    //private float rotationSpeed = 5;
+    private float rotation_x;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public void OnMovevemt(InputAction.CallbackContext action)
     {
         print("YO");
-        print(action);
+
+        transform.position += Vector3.forward;
+
+
+    }
+
+    public void OnRotation(InputAction.CallbackContext action)
+    {
+        rotation_x = action.ReadValue<Vector2>().x;
+        print("rotation");
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        velocity += acc;
+        rb.MovePosition(rb.position + velocity * Time.deltaTime);
+
+        rb.MoveRotation(rb.rotation += rotation_x);
+
+
+
+
+
+
     }
 }
