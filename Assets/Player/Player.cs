cbc20e0f95e361ffc45dc50bd5e5189ea1c937a3
Michael Leonard <michaelleonard5555@gmail.com>
2023-10-28 13:42:13 -0700

    variable flame

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -5,22 +5,48 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public Sprite sprite1;
+    public Sprite sprite2;
+    public Sprite sprite3;
+    public Sprite sprite4;
+
+    [SerializeField]
+    public float dampener = 0.2f;
+
     private float thrustInput;
     private float turnInput;
+    
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    private int thrustAmount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
+        sr = this.GetComponent<SpriteRenderer>();
     }
 
     public void OnMovevemt(InputAction.CallbackContext action)
     {
-        thrustInput = action.ReadValue<Vector2>().y;
-        
-        //float radians = (transform.rotation.eulerAngles.z + 90) * Mathf.Deg2Rad;
-        //acc = new Vector2(Mathf.Cos(radians), Mathf.Sin(radians)) * alpha * action.action.ReadValue<Vector2>().y;
+        if (action.performed)
+        {
+            thrustInput = action.ReadValue<Vector2>().y;
+            thrustAmount += (int)thrustInput;
+            thrustAmount = (int)Mathf.Clamp((float)thrustAmount, 0f, 3f);
+            
+            //float radians = (transform.rotation.eulerAngles.z + 90) * Mathf.Deg2Rad;
+            //acc = new Vector2(Mathf.Cos(radians), Mathf.Sin(radians)) * alpha * action.action.ReadValue<Vector2>().y;
+            if (thrustAmount == 0)
+                sr.sprite = sprite1;
+            if (thrustAmount == 1)
+                sr.sprite = sprite2;
+            if (thrustAmount == 2)
+                sr.sprite = sprite3;
+            if (thrustAmount == 3)
+                sr.sprite = sprite4;
+        }
     }
 
     public void OnRotation(InputAction.CallbackContext action)
@@ -34,7 +60,7 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.AddRelativeForce(Vector2.up * thrustInput);
-         rb.MoveRotation(rb.rotation + -turnInput);
+        rb.AddRelativeForce(Vector2.up * thrustAmount * dampener);
+        rb.MoveRotation(rb.rotation + -turnInput);
     }
 }
