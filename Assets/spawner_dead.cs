4ec32ec921535eb1b51656421e1e8d479357ba02
JaylenLuc <JHLUC@UCI.EDU>
2023-10-29 16:35:06 -0700

    wincond

--- Assets/spawner_dead.cs
+++ Assets/spawner_dead.cs
@@ -7,23 +7,22 @@ public class spawner_dead : MonoBehaviour
     // Start is called before the first frame update
     public GameObject deaddudes;
     public Player player;
-    public 
+    private bool once = false ;
     void Start()
     {
-        player = player.GetComponent<Player>();
         //Instantiate(deaddudes);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player.is_dead) {
-
-            Instantiate(deaddudes,player.transform.position, player.transform.rotation);
-            Instantiate(deaddudes, player.transform.position, player.transform.rotation);
-            Instantiate(deaddudes, player.transform.position, player.transform.rotation);
-            Instantiate(deaddudes, player.transform.position, player.transform.rotation);
-
+        if (once == false && player.is_dead) {
+            
+            Instantiate(deaddudes, new Vector3(player.transform.position.x - .5f, player.transform.position.y - .5f, player.transform.position.z + .5f), player.transform.rotation);
+            Instantiate(deaddudes, new Vector3(player.transform.position.x - .5f, player.transform.position.y + .5f, player.transform.position.z - .5f), player.transform.rotation);
+            Instantiate(deaddudes, new Vector3(player.transform.position.x + .5f, player.transform.position.y + .5f, player.transform.position.z + .5f), player.transform.rotation);
+            Instantiate(deaddudes, new Vector3(player.transform.position.x + .5f, player.transform.position.y - .5f, player.transform.position.z - .5f), player.transform.rotation);
+            once = true;
 
         }
         
