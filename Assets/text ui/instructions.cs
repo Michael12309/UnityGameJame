f42668d49a3bf6a4793fd052a14b3f7b89f22139
Michael <michaelleonard5555@gmail.com>
2023-10-29 16:57:20 -0700

    instructions

--- Assets/text ui/instructions.cs
+++ Assets/text ui/instructions.cs
@@ -5,9 +5,10 @@ using TMPro;
 
 public class instructions : MonoBehaviour
 {
-    public TMP_Text text;  //Add reference to UI Text here via the inspector
-    private float timer = 0f;
-    public float timeToDisappear = 20f;
+    public TMP_Text text;  //Add reference to UI Text here via the inspector    
+    public float timeToDisappear1 = 15f;
+    public float timeToDisappear2 = 27f;
+    public float timeToDisappear3 = 20f;
 
     //Call to enable the text, which also sets the timer
     public void Start()
@@ -18,7 +19,20 @@ public class instructions : MonoBehaviour
     //We check every frame if the timer has expired and the text should disappear
     void Update()
     {
-        print(Time.time);
+        string final = "";
+        if (Time.time < timeToDisappear1)
+        {
+            final = "Your crew is on a mission to visit five distinct planets, each possessing a vital element crucial to their species' survival. Uniting these planetary resources is the key to your interstellar journey home.";
+        }
+        else if (Time.time < timeToDisappear2)
+        {
+            final = "Safely and carefully land on each planet to refuel.";
+        }
+        else
+        {
+            text.enabled = false;
+        }
 
+        text.text = final;
     }
 }
